41cbe338ce82fa9d2572ed456a0bff72a5a9d671
Tim01Bro <arthaos14@gmail.com>
2023-06-20 10:42:33 +0300

    Init commit

---
+++ src/main/io/bookstore/AppBookStore.java
@@ -0,0 +1,11 @@
+package io.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBookStore {
+    public static void main(String[] args) {
+        SpringApplication.run(AppBookStore.class,args);
+    }
+}
