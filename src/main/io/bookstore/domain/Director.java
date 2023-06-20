9da56679b30c28643d356205e81cf1b04e5c3dcb
Tim01Bro <arthaos14@gmail.com>
2023-06-20 10:53:37 +0300

    Director domain

---
+++ src/main/io/bookstore/domain/Director.java
@@ -0,0 +1,23 @@
+package io.bookstore.domain;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+public class Director {
+    private Long id;
+    private String nameDirector;
+    private String sonameDirector;
+    private Integer ageDirector;
+    private Long idStoreDirector;
+    private Date dateGetPositionDirector;
+
+
+}
