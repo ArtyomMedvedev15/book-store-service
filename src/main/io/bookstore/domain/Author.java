cdd1e1fdc834b75aaf1a3fe38edc5bd734caf42f
Tim01Bro <arthaos14@gmail.com>
2023-06-20 10:57:45 +0300

    Author domain

---
+++ src/main/io/bookstore/domain/Author.java
@@ -0,0 +1,23 @@
+package io.bookstore.domain;
+
+
+import lombok.*;
+
+import java.sql.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+public class Author {
+    private Long idAuthor;
+    private String nameAuthor;
+    private String sonameAuthor;
+    private String fanameAuthor;
+    private List<Book>booksAuthor;
+    private Date dateAddToAuthor;
+}
