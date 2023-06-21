ad9e46db46eb657a87f882e0ee42f57c8c6c2c68
Tim01Bro <arthaos14@gmail.com>
2023-06-21 19:07:38 +0300

    Director controller get by id endpoint

--- src/main/io/bookstore/controller/DirectorController.java
+++ src/main/io/bookstore/controller/DirectorController.java
@@ -1,11 +1,13 @@
 package io.bookstore.controller;
 
+import io.bookstore.domain.Director;
 import io.bookstore.service.api.DirectorServiceApi;
 import io.bookstore.util.DirectorResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +22,19 @@ public class DirectorController {
     private final DirectorServiceApi directorServiceApi;
 
     @GetMapping("/all")
-    public ResponseEntity<?>getAllDirector(){
+    public ResponseEntity<?> getAllDirector() {
         var directorDtoList = directorServiceApi.getAll().stream().map(DirectorResponse::fromDomainToDto).toList();
         return ResponseEntity.ok().body(directorDtoList);
+    }
+
+    @GetMapping("/{idDirector}")
+    public ResponseEntity<?> getDirectorById(@PathVariable("idDirector") Long idDirector) {
+        var directorById = directorServiceApi.getById(idDirector);
+        if (directorById != null) {
+            return ResponseEntity.ok().body(DirectorResponse.fromDomainToDto(directorById));
+        } else {
+            return ResponseEntity.badRequest().body(String.format("Director with id %s not found", idDirector));
+        }
     }
 
 
