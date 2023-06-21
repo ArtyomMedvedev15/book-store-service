e1e6d457924d498246bcb1e29bf9934fdb34e346
Tim01Bro <arthaos14@gmail.com>
2023-06-21 19:46:18 +0300

    Store controller get by id with endpoint

--- src/main/io/bookstore/controller/StoreController.java
+++ src/main/io/bookstore/controller/StoreController.java
@@ -7,8 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1/store")
@@ -24,6 +27,19 @@ public class StoreController {
     public ResponseEntity<?>getAllStores(){
         var storeList = storeServiceApi.getAll().stream()
                 .map(o1 -> StoreResponse.fromDomainToDto(o1, directorServiceApi.getById(o1.getIdDirectorStore()))).toList();
+        log.info("Get all store with endpoint in {}", new Date());
         return ResponseEntity.ok().body(storeList);
+    }
+
+    @GetMapping("/{idStore}")
+    public ResponseEntity<?>getStoreById(@PathVariable("idStore")Long idStore){
+        var storeById = storeServiceApi.getById(idStore);
+        if(storeById!=null){
+            log.info("Get store with id {} with endpoint in {}",idStore,new Date());
+            return ResponseEntity.ok().body(StoreResponse.fromDomainToDto(storeById,directorServiceApi.getById(storeById.getIdDirectorStore())));
+        }else{
+            log.error("Store with id {} not found in {}",idStore,new Date());
+            return ResponseEntity.badRequest().body(String.format("Store with id %s not found",idStore));
+        }
     }
 }
